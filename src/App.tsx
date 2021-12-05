b18689ede47eb1ea242e40a1dea8ea25db216168
grind-t <grindt.gd@gmail.com>
2021-12-05 14:02:06 +0300

    style: use prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-  return <div>hello</div>;
+  return <div>hello</div>
 }
 
-export default App;
+export default App

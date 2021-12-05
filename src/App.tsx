8e7e17a50d17e621e69a0ce664e6bdc8972b7bd1
grind-t <grindt.gd@gmail.com>
2021-12-05 14:19:56 +0300

    build: add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div>hello</div>
+  return (
+    <Box>
+      <Typography>Hello</Typography>
+    </Box>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import App from './App'
-import './main.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import CssBaseLine from '@mui/material/CssBaseline'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseLine />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

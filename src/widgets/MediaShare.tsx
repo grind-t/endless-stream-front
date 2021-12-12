f94f7496569149f405151561204e5eb7c42f3e73
grind-t <grindt.gd@gmail.com>
2021-12-12 22:43:40 +0300

    feat: media share progress bar

--- src/widgets/MediaShare.tsx
+++ src/widgets/MediaShare.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@mui/system'
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { LinearProgress, Typography } from '@mui/material'
+import { Box, SxProps, Theme } from '@mui/system'
+import { useEffect, useRef, useState } from 'react'
 import { io } from 'socket.io-client'
 import YTPlayer from 'yt-player'
 
@@ -9,35 +10,67 @@ interface MediaRequest {
   videoTitle: string
 }
 
-function MediaShare() {
+interface MediaShareProps {
+  width?: number
+  height?: number
+  sx?: SxProps<Theme>
+}
+
+function MediaShare({ width, height, sx }: MediaShareProps) {
+  const [videoTitle, setVideoTitle] = useState<string>('')
+  const [progress, setProgress] = useState<number>(0)
   const playerRef = useRef<HTMLElement>()
   const playerElem = playerRef.current
-  const player = useMemo(
-    () =>
-      playerElem &&
-      new YTPlayer(playerElem, {
-        autoplay: true,
-        controls: false,
-        captions: false,
-        annotations: false,
-        related: false,
-      }),
-    [playerElem]
-  )
-  const [media, setMedia] = useState<MediaRequest>()
 
   useEffect(() => {
+    if (!playerElem) return
+    const player = new YTPlayer(playerElem, {
+      width,
+      height,
+      autoplay: true,
+      captions: false,
+      controls: false,
+      annotations: false,
+      modestBranding: true,
+      related: false,
+      timeupdateFrequency: 100,
+    })
     const socket = io()
     socket.on('connect', () => console.log('connected'))
-    socket.on('media/changed', setMedia)
-  }, [])
+    socket.on('media/changed', (req: MediaRequest | undefined) => {
+      if (!req) return
+      player.load(req.videoId, true)
+      setVideoTitle(req.videoTitle)
+    })
+    player.on('timeupdate', (seconds) => {
+      const percentage = seconds / player.getDuration()
+      setProgress(percentage * 100)
+    })
+    player.on('ended', () => socket.emit('media/ended'))
+  }, [playerElem])
 
-  useEffect(() => {
-    if (!player || !media) return
-    player.load(media.videoId, true)
-  }, [player, media])
-
-  return <Box ref={playerRef} />
+  return (
+    <Box sx={{ position: 'relative', lineHeight: 0, ...sx }}>
+      <Box ref={playerRef} />
+      <LinearProgress
+        variant="determinate"
+        color="primary"
+        sx={{ height: 25, opacity: 0.8, boxShadow: 3 }}
+        value={progress}
+      />
+      <Typography
+        sx={{
+          position: 'absolute',
+          bottom: 5,
+          left: 5,
+          lineHeight: 1,
+          color: 'primary.contrastText',
+        }}
+      >
+        {videoTitle}
+      </Typography>
+    </Box>
+  )
 }
 
 export default MediaShare

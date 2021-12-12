402bddeb9452c44094ce49ae860abeaf1b0c5b2a
grind-t <grindt.gd@gmail.com>
2021-12-13 00:52:39 +0300

    feat: brightness transition when media is playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import backgroundUrl from './background.png'
 import MediaShare from './widgets/MediaShare'
 import { Box } from '@mui/system'
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 
 function App() {
+  const [isMediaPlaying, setIsMediaPlaying] = useState<boolean>(false)
+
+  useEffect(() => {
+    const socket = io()
+    socket.on('media/changed', (req) => setIsMediaPlaying(!!req))
+  }, [])
+
   return (
     <Box
       sx={{
@@ -19,7 +28,8 @@ function App() {
           backgroundImage: `url(${backgroundUrl})`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
-          filter: 'brightness(0.9)',
+          filter: isMediaPlaying ? 'brightness(0.5)' : undefined,
+          transition: 'filter 1s',
         },
       }}
     >

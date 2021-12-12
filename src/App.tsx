974a554039e6246d60587eeb6f17a44a5cf75659
grind-t <grindt.gd@gmail.com>
2021-12-12 22:40:46 +0300

    feat: background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import backgroundUrl from './background.png'
 import MediaShare from './widgets/MediaShare'
+import { Box } from '@mui/system'
 
 function App() {
-  return <MediaShare />
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+        '::before': {
+          content: '""',
+          position: 'fixed',
+          width: 'inherit',
+          height: 'inherit',
+          backgroundImage: `url(${backgroundUrl})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          filter: 'brightness(0.9)',
+        },
+      }}
+    >
+      <MediaShare width={854} height={480} />
+    </Box>
+  )
 }
 
 export default App

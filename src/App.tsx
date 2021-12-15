651bc9d42875322c8c2a4d0318b6648013c02275
grind-t <grindt.gd@gmail.com>
2021-12-15 15:28:36 +0300

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import backgroundUrl from './background.png'
+import EventList from './widgets/EventList'
 import MediaShare from './widgets/MediaShare'
 import { Box } from '@mui/system'
 import { useEffect, useState } from 'react'
@@ -16,6 +17,13 @@ function App() {
     <Box
       sx={{
         display: 'grid',
+        gridTemplateAreas: `
+        ". . ." 
+        ". media events" 
+        ". . events"
+        `,
+        gridTemplateColumns: '1fr auto 1fr',
+        gridTemplateRows: '1fr auto 1fr',
         justifyContent: 'center',
         alignItems: 'center',
         width: '100vw',
@@ -30,10 +38,12 @@ function App() {
           backgroundSize: 'cover',
           filter: isMediaPlaying ? 'brightness(0.5)' : undefined,
           transition: 'filter 1s',
+          zIndex: -1,
         },
       }}
     >
-      <MediaShare width={854} height={480} />
+      <MediaShare width={1280} height={720} sx={{ gridArea: 'media' }} />
+      <EventList sx={{ gridArea: 'events', mt: 'auto', mr: 3, ml: 'auto' }} />
     </Box>
   )
 }

993ac0e8621b8cd61a570f35625b0cfe95387455
grind-t <grindt.gd@gmail.com>
2021-12-15 15:27:21 +0300

    feat: dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,23 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { createTheme, ThemeProvider } from '@mui/material'
 import CssBaseLine from '@mui/material/CssBaseline'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseLine />
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseLine />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )

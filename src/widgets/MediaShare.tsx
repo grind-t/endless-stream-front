7cbae16af717a994f24f92dfb4c0f0d6d89177d5
grind-t <grindt.gd@gmail.com>
2021-12-23 23:39:51 +0300

    feat(media-share): playlists

--- src/widgets/MediaShare.tsx
+++ src/widgets/MediaShare.tsx
@@ -1,12 +1,15 @@
-import clsx from 'clsx'
 import { useCallback, useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
-import YTPlayer from 'yt-player'
 
-interface MediaRequest {
-  user: string
-  videoId: string
-  videoTitle: string
+enum MediaType {
+  Video,
+  Playlist,
+}
+
+interface Media {
+  id: string
+  title: string
+  type: MediaType
 }
 
 interface MediaShareProps {
@@ -15,48 +18,81 @@ interface MediaShareProps {
   className?: string
 }
 
-function MediaShare({ width, height, className }: MediaShareProps) {
-  const [player, setPlayer] = useState<YTPlayer>()
-  const [videoTitle, setVideoTitle] = useState<string>('')
-  const [progress, setProgress] = useState<number>(0)
-  const [isVisible, setIsVisible] = useState<boolean>(false)
+function loadYT(): Promise<void> {
+  const isLoaded = window.YT && typeof window.YT.Player === 'function'
+  if (isLoaded) return Promise.resolve()
+  const loading = new Promise<void>((resolve) => {
+    // @ts-ignore
+    const prevHandler = window.onYouTubeIframeAPIReady
+    // @ts-ignore
+    window.onYouTubeIframeAPIReady = () => {
+      if (typeof prevHandler === 'function') prevHandler()
+      resolve()
+    }
+  })
+  const apiUrl = 'https://www.youtube.com/iframe_api'
+  let script = document.querySelector<HTMLScriptElement>(
+    `script[src='${apiUrl}']`
+  )
+  if (script) return loading
+  script = document.createElement('script')
+  script.src = apiUrl
+  const firstScript = document.querySelector('script')
+  // @ts-ignore
+  firstScript.parentNode.insertBefore(script, firstScript)
+  return loading
+}
 
-  const initPlayer = useCallback((playerNode) => {
+function MediaShare({ width, height, className }: MediaShareProps) {
+  const [player, setPlayer] = useState<YT.Player>()
+  const [media, setMedia] = useState<Media>()
+  const [progress, setProgress] = useState<number>(0)
+
+  const initPlayer = useCallback(async (playerNode) => {
     if (!playerNode) return
-    const player = new YTPlayer(playerNode, {
+    await loadYT()
+    const player = new YT.Player(playerNode, {
       width,
       height,
-      autoplay: true,
-      captions: false,
-      controls: false,
-      annotations: false,
-      modestBranding: true,
-      related: false,
       host: 'https://www.youtube-nocookie.com',
-      timeupdateFrequency: 100,
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        enablejsapi: 1,
+        iv_load_policy: 3,
+        modestbranding: 1,
+        rel: 0,
+        origin: window.location.origin,
+      },
     })
-    setPlayer(player)
+    const handlePlayerReady = () => {
+      player.removeEventListener('onReady', handlePlayerReady)
+      setPlayer(player)
+    }
+    player.addEventListener('onReady', handlePlayerReady)
   }, [])
 
   useEffect(() => {
     if (!player) return
     const socket = io()
-    const handleMediaChange = (req?: MediaRequest) => {
-      if (!req) return setIsVisible(false)
-      player.load(req.videoId, true)
-      setVideoTitle(req.videoTitle)
-    }
-    player.on('buffering', () => setIsVisible(true))
-    player.on('timeupdate', (seconds) => {
-      const percentage = seconds / player.getDuration()
+    const timerId = setInterval(() => {
+      const percentage = player.getCurrentTime() / player.getDuration()
       setProgress(percentage * 100)
-    })
-    player.on('ended', () => socket.emit('media/ended'))
-    player.on('unplayable', () => socket.emit('media/ended'))
-    socket.on('media/changed', handleMediaChange)
+    }, 100)
+    const handleStateChange = (e: YT.OnStateChangeEvent) => {
+      if (e.data === YT.PlayerState.ENDED) socket.emit('media/ended')
+    }
+    const handleError = () => {
+      socket.emit('media/ended')
+    }
+    player.addEventListener('onStateChange', handleStateChange)
+    player.addEventListener('onError', handleError)
+    socket.on('media/changed', setMedia)
     return () => {
-      socket.off('media/changed', handleMediaChange)
-      player.destroy()
+      clearInterval(timerId)
+      player.removeEventListener('onStateChange', handleStateChange)
+      player.removeEventListener('onError', handleError)
+      socket.off('media/changed', setMedia)
     }
   }, [player])
 
@@ -65,15 +101,21 @@ function MediaShare({ width, height, className }: MediaShareProps) {
     player.setSize(width, height)
   }, [player, width, height])
 
+  useEffect(() => {
+    if (!player || !media) return
+    if (media.type === MediaType.Video) player.loadVideoById(media.id)
+    else player.loadPlaylist({ listType: 'playlist', list: media.id })
+  }, [player, media])
+
   return (
-    <div className={clsx(isVisible ? 'visible' : 'invisible', className)}>
+    <div className={className}>
       <div ref={initPlayer} />
       <div className="absolute left-0 bottom-0 bg-slate-400">
         <div
           className="px-2 bg-gradient-to-r from-slate-50 to-slate-50 bg-no-repeat font-semibold transition-all duration-100 ease-linear"
           style={{ backgroundSize: progress + '%' }}
         >
-          {videoTitle}
+          {media && media.title}
         </div>
       </div>
     </div>
